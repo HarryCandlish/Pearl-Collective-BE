d4eee4327b3e4b8d7393a7db5ca8f1105c6530f4
HarryCandlish <hcnd90@gmail.com>
2019-08-11 17:06:48 +1200

    sda

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Nav from "./layout/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
