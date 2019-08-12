e98b467684aa7e4e27a33abd099545dcc54a2933
HarryCandlish <hcnd90@gmail.com>
2019-08-12 14:54:49 +1200

    basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Nav from "./layout/Nav";
+// import Nav from "./layout/Nav";
 
 function App() {
   return (
